8ef6b7bcd68aeb3602be5eafc422ff111e7d4687
maitri-dalvi <maitridalvi2004@gmail.com>
2025-05-29 02:45:18 +0530

    added css

---
+++ src/Pokemon.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import "./index.css";
+const Pokemon = () => {
+
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
+    const fetchPokemon = async () => {
+        try {
+            const res = await fetch(API);
+            const data = await res.json();
+
+
+            // fetched 1000+ urls (url inside url)
+            const pokemonDetailedData = data.results.map( async (currPokemon) => {
+                const res = await fetch(currPokemon.url);
+                const data = await res.json();
+                return data;                
+            });
+            console.log(pokemonDetailedData);
+            
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
+  return (
+    <>
+    </>
+  )
+}
+
+export default Pokemon
